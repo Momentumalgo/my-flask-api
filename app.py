b670745270cb300fc231a2795f69029ec91ddde5
Momentumalgo <momentumalgo@gmail.com>
2025-01-16 01:21:25 +1300

    Adjusted Flask API to return server time in UTC+2

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask, request, jsonify
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
-
-# Root route
-@app.route('/')
-def home():
-    return "Welcome to the Flask API!"
 
 # Route to handle trade data from EA
 @app.route('/trade_data', methods=['POST'])
@@ -13,10 +9,15 @@ def trade_data():
     data = request.get_json()
     print("Received data from EA:", data)
 
+    # Adjust time to MetaTrader server time (assumed UTC+2)
+    server_time = datetime.utcnow() + timedelta(hours=2)
+    print("Server Time (UTC+2):", server_time)
+
     # Example response to the EA
     response = {
         "command": "AdjustRisk",
-        "new_risk_percent": 0.5
+        "new_risk_percent": 0.5,
+        "server_time": server_time.strftime("%Y-%m-%d %H:%M:%S")
     }
     return jsonify(response)
 
